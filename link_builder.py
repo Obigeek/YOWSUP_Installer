26f5c10af89dc87840f384e44267218e153e1476
Joe Izzard <Joe@Izzard.me>
2017-11-22 16:53:52 +0000

    Link Builder update

--- link_builder.py
+++ link_builder.py
@@ -2,7 +2,7 @@ from BeautifulSoup import BeautifulSoup
 import urllib2
 import re
 
-wapage = urllib2.urlopen('https://www.whatsapp/com/android')
-soup = BeautifulSoup(wapage)
-for link in soup.findAll('a', attrs={'href': re.compile("^https://")});
+html_page = urllib2.urlopen("https://www.whatsapp.com/android/")
+soup = BeautifulSoup(html_page)
+for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
     print link.get('href')
